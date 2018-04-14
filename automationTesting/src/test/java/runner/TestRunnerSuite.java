f835168eab8d4244dab18d084087448a7c908115
github1sakthi <38174109+github1sakthi@users.noreply.github.com>
2018-04-14 18:56:32 -0500

    Action, TakeScreenShot

--- automationTesting/src/test/java/runner/TestRunnerSuite.java
+++ automationTesting/src/test/java/runner/TestRunnerSuite.java
@@ -6,10 +6,10 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-//@CucumberOptions(features = "features", glue = { "stepDefinitions" }, tags = { "@WindowHandlerTest,@GmailLoginTest,@OrbitzFlightSearchTest,@QuestDiagnosisAppointmentBookingTest" }, plugin = { "pretty",
-	//	"html:target/cucumber-reports"} , monochrome=true)
-@CucumberOptions(features = "features", glue = { "stepDefinitions" }, tags = { "@OrbitzFlightSearchTest" }, plugin = { "pretty",
-"html:target/cucumber-reports"} , monochrome=true)
+@CucumberOptions(features = "features", glue = { "stepDefinitions" }, tags = { "@AdvancedTest,@WindowHandlerTest,@GmailLoginTest,@OrbitzFlightSearchTest,@QuestDiagnosisAppointmentBookingTest" }, plugin = { "pretty",
+		"html:target/cucumber-reports"} , monochrome=true)
+//@CucumberOptions(features = "features", glue = { "stepDefinitions" }, tags = { "@AdvancedTest" }, plugin = { "pretty",
+//"json:target/json/testreport.json","html:target/html/native-cucumber-reports.html","junit:target/junitt/testreport.xml"} , monochrome=true)
 
 public class TestRunnerSuite {
 
